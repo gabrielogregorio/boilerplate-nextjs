2dec148ad5591369501df320125211bde3722653
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2021-11-20 21:49:53 -0300

    ajustado eslint para trabalhar com o typescript mais recente

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,12 @@
+import { Post } from '../../components/Post'
 import styles from './styles.module.css'
 
 export default function Home () {
   return  (
     <div className={styles.home}>
       <h1>oi</h1>
+
+      <Post />
     </div>
   )
 }
