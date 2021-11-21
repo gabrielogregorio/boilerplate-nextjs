52d9eba92d1d74f4368b1af408bdc2e2e60bd53d
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2021-11-20 22:46:42 -0300

    adicionado o prettier

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,5 +1,3 @@
 export const Post = () => {
-  return <div>
-    Post 1
-  </div>
+  return <div>Post 1</div>
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,10 @@
-import {Post} from '../../components/Post'
+import { Post } from '../../components/Post'
 import styles from './styles.module.css'
 
-export default function Home () {
-  return  (
+export default function Home() {
+  return (
     <div className={styles.home}>
       <h1>oi</h1>
-
       <Post />
     </div>
   )
